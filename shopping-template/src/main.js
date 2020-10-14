887fbd033ca131541817baca3e19ec729de18257
jianna-0419 <qqqezwon2@gmail.com>
2020-10-14 16:50:40 +0900

    finish shopping-template

--- shopping-template/src/main.js
+++ shopping-template/src/main.js
@@ -6,25 +6,48 @@ function loadItems() {
         .then(json => json.items);
 }
 
+// Update the list with the given items
 function displayItems(items) {
     const container = document.querySelector('.items');
-    container.innerHTML = items.map(item => createHTMLString(item));
+    const html = items.map(item => createHTMLString(item)).join('');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
+// Create HTML item from the given data items
 function createHTMLString(item) {
     return `
     <li class="item">
-        <img src="${item.image}" alt="tshirt" class="item__thumbnail">
-        <span class="item__decription">male, large</span>
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__decription">${item.gender}, ${item.size}</span>
     </li>
     `;
+}
+
+// Handle button click
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;     
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.Btn-menu');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
 }
 
 // main
 loadItems()
     .then(items => {
-        console.log(items);
         displayItems(items);
-        // setEventListeners(items);
+        setEventListeners(items);
     })
     .catch(console.log);
