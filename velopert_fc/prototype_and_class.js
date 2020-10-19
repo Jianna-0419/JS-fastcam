4e148b00a9466e690d84250d85ba8b43cbee09c5
jianna-0419 <qqqezwon2@gmail.com>
2020-10-20 01:11:28 +0900

    end of 2020/10/19

--- velopert_fc/prototype_and_class.js
+++ velopert_fc/prototype_and_class.js
@@ -1,6 +1,8 @@
 // 프로토 타입과 객체 
 
 // 객체 생성자
+
+// class 생기기 전 !
     function Animal(type, name, sound) {
         this.type = type;
         this.name = name;
@@ -38,12 +40,49 @@
     dog.say();
     cat.say();
 
-// prototype
+// prototype  상속 느낌
     // 객체 생성자로 새로운 객체들을 새로 만들었을 때
     // 그 객체들 끼리 공유할 수 있는 값이나 함수를 만드는 것
     // 객체 안에 넣어서 적는 거랑 Animal.prototype.say 랑 똑같다 
 
+    console.log('---------------');
 
+// 클래스 
+    // constructor : 생성자 라는 의미
 
-// 상속받기 !
+        class Animal2 {
+            constructor(type, name, sound) {
+                this.type = type;
+                this.name = name;
+                this.sound = sound;
+            }
+            say() {
+                console.log(this.sound);
+            }
+                // 클래스 내부에 함수는 prototype 으로 자동으로 등록된다
+        }
+
+        console.log(Animal2.prototype.say);     // say 함수 출력
+
+    // extends
+        // 상속받기
+
+        class Dog2 extends Animal2 {
+            // Animal2 에 있는 설정들을 가지고 있게됨
+            // constructor 만 써주면 된다 
+            constructor(name, sound) {
+                super('개', name, sound);
+            }
+        }
     
+        class Cat2 extends Animal2 {
+            constructor(name, sound) {
+                super('고양이', name, sound);
+            }
+        }
+
+        const dog2 = new Dog2('멍멍이', '왈왈');
+        const cat2 = new Cat2('고양이', '미야옹');
+
+        dog2.say();
+        cat2.say();
