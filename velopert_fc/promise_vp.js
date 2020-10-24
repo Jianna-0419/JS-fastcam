498d4aa8dd994fd2324f339bb64e9309db186237
jianna-0419 <qqqezwon2@gmail.com>
2020-10-24 22:40:03 +0900

    finished promise vp

--- velopert_fc/promise_vp.js
+++ velopert_fc/promise_vp.js
@@ -59,7 +59,7 @@ console.log('Promise vp 연결 확인했다요!');
             });
         }
 
-        increaseAndPrint2(0)
+        /* increaseAndPrint2(0)
             // .then(n => {            // Promise의 resolve 된 값을 n 으로 받아오는 것!
             //     console.log('result: ', n);
             // })
@@ -68,7 +68,7 @@ console.log('Promise vp 연결 확인했다요!');
             .then(increaseAndPrint2)
             .then(increaseAndPrint2)
             .then(increaseAndPrint2)
-            .catch(error => {console.error(error);})
+            .catch(error => {console.error(error);}) */
             // 코드가 간단하게 작성된다!
             // But! 어디서 오류가 날지 catch 를 써야할 지 모르는 상황에서는 사용이 어렵고
             // 중간에 만약 다른 작업을 넣어야 하거나 이런 경우에도 어렵다 
@@ -89,4 +89,49 @@ console.log('Promise vp 연결 확인했다요!');
 
         process();          안녀하세요 반값습니다 차례대로 출력
 
+    // 화살표 함수
+        const getDog = async (D => {
+
+        })
+
 */
+
+    // Promise await
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        const getDog = async () => {
+            await sleep(1000);
+            return '멍멍이';
+        }
+
+        const getRabbit = async () => {
+            await sleep(500);
+            return '토끼';
+        }   // 가장 빨리 끝남
+
+        const getTurtle = async () => {
+            await sleep(3000);
+            return '거북이';
+        }
+
+        async function process() {
+           /*  const results = await Promise.all([getDog(), getRabbit(), getTurtle()]);
+            console.log(results); */
+
+            // 각각 변수로 따오기 
+            const [dog, rabbit, turtle] = await Promise.all([getDog(), getRabbit(),getTurtle()]);
+            console.log('1 ' + dog);
+            console.log('2 ' + rabbit);
+            console.log('3 ' + turtle);
+
+            // Promise.race   =   가장 빨리 끝난 값 리턴 
+            const first = await Promise.race([getDog(), getRabbit(), getTurtle()]);
+            console.log(first);         // 토끼 출력 
+        }
+
+        process();      //   ['멍멍이', '토끼', '거북이']
+
+        // Promise.all  =  배열 안의 값(혹은 결과값)을 배열의 형태로 전달
+        // Promise.race =  배열 중에서 가장 빨리 끝나는 함수 결과값만 전달
